f9494759dacba92c9d4709ecbe3c977393283ccc
1-designer-1 <sereogin.denis20151@gmail.com>
2021-03-10 12:22:05 +0300

    Добавьте файлы проекта.

---
+++ Task2 mdk 01.02/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TaskLib;
+
+namespace Task2_mdk_01._02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void закрытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void очиститьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            textBox6.Clear();
+        }
+
+        private void ответToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void оПрограммеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Выполнил студент: Серегин Денис\n" +
+                "Вариант 11\n" +
+                "Задание: " +
+                "Даны прямоугольное отверстие размером x на y и кирпич с гранями a, b, c. Составить " +
+                "программу для определения возможности прохождения кирпича через заданное отверстие.",
+                "О программе", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
