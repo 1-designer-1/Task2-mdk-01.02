3bcc6a53f6f7d1b5aab56173d7de44f995aa3491
1-designer-1 <sereogin.denis20151@gmail.com>
2021-03-12 10:37:16 +0300

    тест сделать

--- Task2 mdk 01.02/Form1.cs
+++ Task2 mdk 01.02/Form1.cs
@@ -46,16 +46,22 @@ namespace Task2_mdk_01._02
 
         private void ответToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            int firstNumber = Convert.ToInt32(textBox1.Text);  // гранями a
-            int secondNumber = Convert.ToInt32(textBox2.Text); // гранями b
-            int thirdNumber = Convert.ToInt32(textBox3.Text); //гранями  c
-            int longPort = Convert.ToInt32(textBox4.Text); // размером отверстия х
-            int widthPort = Convert.ToInt32(textBox5.Text); // размером отверстия  у
-            if(Class1.Check(firstNumber, secondNumber, thirdNumber, longPort,widthPort) == true)
+            try
             {
-                textBox6.Text = "Kирпича проходит через заданное отверстие";
+                int firstNumber = Convert.ToInt32(textBox1.Text);  // гранями a
+                int secondNumber = Convert.ToInt32(textBox2.Text); // гранями b
+                int thirdNumber = Convert.ToInt32(textBox3.Text); //гранями  c
+                int longPort = Convert.ToInt32(textBox4.Text); // размером отверстия х
+                int widthPort = Convert.ToInt32(textBox5.Text); // размером отверстия  у
+                if (Class1.Check(firstNumber, secondNumber, thirdNumber, longPort, widthPort) == true)
+                {
+                    textBox6.Text = "Kирпича проходит через заданное отверстие";
+                }
+                else textBox6.Text = "Kирпича не проходит через заданное отверстие";
             }
-            else textBox6.Text = "Kирпича не проходит через заданное отверстие";
+            catch { MessageBox.Show("Введите число", "Ошибка",MessageBoxButtons.OK,MessageBoxIcon.Error); }
+
+
 
 
 
---
+++ UnitTest1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest1")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5e1a141a-8ae4-4ed8-adbe-bc5ed2cd86b7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
