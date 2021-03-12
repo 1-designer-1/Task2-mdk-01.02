10e63c42dd36b6917998f6af15e1180e987084bb
1-designer-1 <sereogin.denis20151@gmail.com>
2021-03-12 10:02:17 +0300

    1

--- TaskLib/Class1.cs
+++ TaskLib/Class1.cs
@@ -8,7 +8,29 @@ namespace TaskLib
 {
     public class Class1
     {
-        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="longRectangle"> Длина кирпича</param>
+        /// <param name="widthRectangle">Ширина кирпича</param>
+        /// <param name="heightRectangle">Высота кирпича</param>
+        /// <param name="longPort">Длина отверстия</param>
+        /// <param name="widthPort">Ширина отверстия</param>
+        /// <returns></returns>
+        public bool Check(int longRectangle, int widthRectangle, int heightRectangle, int longPort, int widthPort)
+        {
+            //int s_otv = longPort * widthPort;
+            if ((longPort == longRectangle && widthPort == widthRectangle) || (widthRectangle == longPort && longRectangle == widthPort) 
+                || (longPort == widthRectangle && widthPort == heightRectangle) || (longPort == heightRectangle && widthPort == longRectangle) || (longPort == heightRectangle && widthPort  == widthRectangle) || (longPort == longRectangle && widthPort == heightRectangle))
+            {
+                return true;
+            }
+            else return false;
+
+
+
+
+        }
 
     }
 }
